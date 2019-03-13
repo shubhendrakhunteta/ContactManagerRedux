6f4934012026610fe4d132da1b8d4cf499645d1b
Shubhendra Khunteta <shubhendrakhunteta@gmail.com>
2019-03-13 16:12:20 +0530

    Using SpringBootTestApplication for data

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -8,7 +8,7 @@ import {
 import axios from "axios";
 
 export const getContacts = () => async dispatch => {
-  const res = await axios.get("http://jsonplaceholder.typicode.com/users");
+  const res = await axios.get("localhost:8080/person/");
   dispatch({
     type: GET_CONTACTS,
     payload: res.data
@@ -16,9 +16,7 @@ export const getContacts = () => async dispatch => {
 };
 
 export const getContact = id => async dispatch => {
-  const res = await axios.get(
-    `http://jsonplaceholder.typicode.com/users/${id}`
-  );
+  const res = await axios.get(`localhost:8080/person/${id}`);
   dispatch({
     type: GET_CONTACT,
     payload: res.data
@@ -27,7 +25,7 @@ export const getContact = id => async dispatch => {
 
 export const deleteContact = id => async dispatch => {
   try {
-    await axios.delete(`http://jsonplaceholder.typicode.com/users/${id}`);
+    await axios.delete(`localhost:8080/person/${id}`);
     dispatch({
       type: DELETE_CONTACT,
       payload: id
@@ -41,10 +39,7 @@ export const deleteContact = id => async dispatch => {
 };
 
 export const addContact = contact => async dispatch => {
-  const res = await axios.post(
-    "http://jsonplaceholder.typicode.com/users",
-    contact
-  );
+  const res = await axios.post("localhost:8080/person/", contact);
   dispatch({
     type: ADD_CONTACT,
     payload: res.data
@@ -52,10 +47,7 @@ export const addContact = contact => async dispatch => {
 };
 
 export const updateContact = contact => async dispatch => {
-  const res = await axios.put(
-    `http://jsonplaceholder.typicode.com/users/${contact.id}`,
-    contact
-  );
+  const res = await axios.put(`localhost:8080/person/${contact.id}`, contact);
   dispatch({
     type: UPDATE_CONTACT,
     payload: res.data
